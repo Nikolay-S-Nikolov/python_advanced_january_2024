e60b6349a9defef78e7134b317c7807dcf5713a7
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-12-29 17:46:29 +0200

    Problem 05. Primary Diagonal

--- 8_multidimensional_lists/05_primary_diagonal.py
+++ 8_multidimensional_lists/05_primary_diagonal.py
@@ -1,6 +1,7 @@
 int_n = int(input())
 
 matrix = [[int(x) for x in input().split()] for _ in range(int_n)]
+# matrix = [list(map(int, input().split())) for _ in range(int_n)]
 
 # for i_j in range(int_n):
 # #     sum_primary_diagonal += matrix[i_j][i_j]
