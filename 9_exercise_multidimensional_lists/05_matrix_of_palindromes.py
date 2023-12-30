5851060d81ea72fca110bb38efe6670d1eb571b4
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2023-12-30 14:52:07 +0200

    Problem 05. Matrix of Palindromes

--- 9_exercise_multidimensional_lists/05_matrix_of_palindromes.py
+++ 9_exercise_multidimensional_lists/05_matrix_of_palindromes.py
@@ -3,8 +3,8 @@ rows, cols = [int(x) for x in input().split()]
 matrix = []
 for row in range(rows):
     matrix.append([])
+    first_last_letter = chr(ord('a') + row)
     for col in range(cols):
-        first_last_letter = chr(ord('a') + row)
         middle_letter = chr(ord(first_last_letter) + col)
         matrix[row].append(first_last_letter + middle_letter + first_last_letter)
 
