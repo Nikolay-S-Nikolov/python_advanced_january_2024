3eb4cbfd8c6a8c20bb9b09076658b73d8267b0d3
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-06 16:52:19 +0200

    Problem 06. Function Executor

---
+++ 12_exercise_functions_advanced/06_function_executor.py
@@ -0,0 +1,37 @@
+def func_executor(*funcs):
+    results = []
+
+    for func, arg in funcs:
+        results.append(f'{func.__name__} - {func(*arg)}')
+
+    return "\n".join(results)
+
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+print(func_executor(
+    (sum_numbers, (1, 2)),
+    (multiply_numbers, (2, 4))
+))
+
+
+def make_upper(*strings):
+    result = tuple(s.upper() for s in strings)
+    return result
+
+
+def make_lower(*strings):
+    result = tuple(s.lower() for s in strings)
+    return result
+
+
+print(func_executor(
+    (make_upper, ("Python", "softUni")),
+    (make_lower, ("PyThOn",)),
+))
