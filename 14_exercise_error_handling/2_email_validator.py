5223024c0ee76f8a9420a3e9c6eaffa4a5b4fa34
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-20 20:42:41 +0200

    Problem 2.	Email Validator

---
+++ 14_exercise_error_handling/2_email_validator.py
@@ -0,0 +1,40 @@
+from re import findall
+
+
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+class TooManyAtSymbolError(Exception):
+    pass
+
+
+class NameContainSymbolsError(Exception):
+    pass
+
+
+NAME_LENGTH = 4
+VALID_DOMAIN = (".com", ".bg", ".net", ".org")
+
+pattern = r'\.?\w+'
+email = input()
+while email != "End":
+    if "@" not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+    if len(email.split("@")[0]) <= 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+    if not email.split("@")[0] == findall(pattern, email)[0]:
+        raise NameContainSymbolsError("Name can contain only letters and underscore")
+    if findall(pattern, email)[-1] not in VALID_DOMAIN:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org, .net")
+    print("Email is valid")
+
+    email = input()
