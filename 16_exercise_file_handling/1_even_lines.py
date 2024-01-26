52ff32969384a8d2eda8924fa61dd7c6138fda42
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-26 19:58:46 +0200

    Problem 1. Even Lines

---
+++ 16_exercise_file_handling/1_even_lines.py
@@ -0,0 +1,15 @@
+import os
+
+WORKING_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(WORKING_DIRECTORY_PATH, 'files', 'text.txt')
+
+symbols = ["-", ",", ".", "!", "?"]
+
+with open(file_path) as file:
+    text = file.readlines()
+
+for row in range(0, len(text), 2):
+
+    for symbol in symbols:
+        text[row] = text[row].replace(symbol, '@')
+    print(*text[row].split()[::-1])
