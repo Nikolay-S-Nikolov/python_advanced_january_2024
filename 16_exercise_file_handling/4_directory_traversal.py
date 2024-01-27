ae9bfdc114735bd02d89563f783b4c4f7e133e67
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-27 18:30:32 +0200

    Problem 4. Directory Traversal

---
+++ 16_exercise_file_handling/4_directory_traversal.py
@@ -0,0 +1,35 @@
+import os
+
+
+def find_files(dir_name, first_level=False):
+    for file_or_dir in os.listdir(dir_name):
+        file_name = os.path.join(dir_name, file_or_dir)
+        print(file_name)
+
+        if os.path.isfile(file_name):
+            file_extension = file_name.split('.')[-1]
+
+            if file_extension not in files_grouped_by_extension:
+                files_grouped_by_extension[file_extension] = []
+
+            files_grouped_by_extension[file_extension].append(f"- - - {file_or_dir}")
+
+        elif os.path.isdir(file_name) and not first_level:
+            find_files(file_name, first_level=True)
+
+
+directory = input()
+files_grouped_by_extension = {}
+
+try:
+    find_files(directory)
+
+except FileNotFoundError:
+    WORKING_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
+    searched_path = os.path.join(WORKING_DIRECTORY_PATH, directory)
+    print(f"The system cannot find the specified path: {searched_path}")
+
+for extension, files in sorted(files_grouped_by_extension.items(), key=lambda x: (x[0], x[1])):
+    print(f'.{extension}', sep='\n')
+    for file in files:
+        print(f'{file}', sep='\n')
