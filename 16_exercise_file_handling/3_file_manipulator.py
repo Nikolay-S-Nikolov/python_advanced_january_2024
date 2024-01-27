0e65501b9161061e5187836e7c5a6d9b908a47cb
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-27 15:50:27 +0200

    Problem 3. File Manipulator

---
+++ 16_exercise_file_handling/3_file_manipulator.py
@@ -0,0 +1,49 @@
+import os
+
+WORKING_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def create_file(path):
+    with open(path, 'w'):
+        pass
+
+
+def add_content(path, data):
+    with open(path, 'a') as file:
+        file.write(f'{data}\n')
+
+
+def replace_string(path, *data):
+    old_string, new_string = data
+    try:
+        with open(path, 'r+') as file:
+            text = file.read()
+            file.seek(0)
+            file.write(text.replace(old_string, new_string))
+
+    except FileNotFoundError:
+        print("An error occurred")
+
+
+def delete_file(path):
+    try:
+        os.remove(path)
+    except FileNotFoundError:
+        print("An error occurred")
+
+
+functions = {
+    'Create': create_file,
+    'Add': add_content,
+    'Replace': replace_string,
+    'Delete': delete_file
+}
+
+command = input().split('-')
+while command[0] != 'End':
+    action, file_name, *info = command
+    file_path = os.path.join(WORKING_DIRECTORY_PATH, 'files', f'{file_name}')
+
+    functions[action](file_path, *info)
+
+    command = input().split('-')
