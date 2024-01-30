c55d7ea645a6cf0059f88949937ac1de075435f6
nikolasy.s.nikolov@gmail.com <nikolay.s.nikolov@gmail.com>
2024-01-30 16:37:18 +0200

    Problem 2. ASCII Art

--- 19_modules/2_ascii_art.py
+++ 19_modules/2_ascii_art.py
@@ -1,12 +1,21 @@
-from pyfiglet import figlet_format
+from pyfiglet import figlet_format, print_figlet
 
 
 def print_art_word(msg):
-    ascii_art = figlet_format(msg)
+    ascii_art = print_figlet(msg, font='bubble')
+    # font = "5lineoblique" , "3x5" , "3-d", "slant","alphabet", "banner3-D","doh", "isometric1","letters"
+    # "bubble", "bulbhead", "digital",
     return ascii_art
 
 
-print(print_art_word(input()))
+# def print_art_with_print_fig(msg):
+#     ascii_art = figlet_format(msg)
+#     return ascii_art
 
+
+message = input()
+
+print_art_word(message)
+# print(print_art_with_print_fig(message))
 
 # pip install pyfiglet
